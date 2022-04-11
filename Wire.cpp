e4189924cc2b0cb424a9a1cdb5e6043be054cd52
arrxnov <jcbgrd@gmail.com>
2022-04-11 08:14:45 -0400

    Added Wire.h, Wire.cpp, main.cpp

---
+++ Wire.cpp
@@ -0,0 +1,35 @@
+#include "Wire.h"
+
+Wire::Wire(char* n)
+{
+    name = n;
+}
+
+void Wire::appendHist(int h, int time)
+{
+    for (int i = 0; i < time; i++) history.push_back(h);
+}
+
+void Wire::editDrives(Gate* g, int e=ADD)
+{
+    if (e == ADD) drives.push_back(g);
+    else if (e == REMOVE)
+    {
+        for (int i = 0; i < drives.size(); i++)
+        {
+            if (drives.at(i) == g)
+            {
+                auto iterator = drives.begin();
+                drives.erase(iterator+i);
+            }
+        }
+    }
+}
+
+void Wire::printHistory() const
+{
+    for (int i = 0; i < history.size(); i++)
+    {
+        std::cout << history.at(i) << " ";
+    }
+}
---
+++ Wire.h
@@ -0,0 +1,30 @@
+#ifndef WIRE_H
+#define WIRE_H
+#define REMOVE 0
+#define ADD 1
+#include "Gate.h"
+#include <vector>
+#include <iostream>
+
+class Wire
+{
+    private:
+        bool value;
+        char* name;
+        std::vector <Gate*> drives;
+        int index;
+        std::vector<int> history;
+    public:
+        Wire(char* n);
+        void setValue(int v) { value = v; };
+        void appendHist(int h, int time);
+        void editDrives(Gate* g, int e);
+        int getValue() const { return value; };
+        char* getName() const { return name; };
+        std::vector<Gate*> getDrives() const { return drives; };
+        int getIndex() const { return index; };
+        std::vector<int> getHistory() const { return history; };
+        void printHistory() const;
+};
+
+#endif
