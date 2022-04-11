17059ce26c953736377bc4bf72100f522ee88ecb
arrxnov <jcbgrd@gmail.com>
2022-04-11 08:59:59 -0400

    Added Gate.* and bitwise math

---
+++ Gate.cpp
@@ -0,0 +1,134 @@
+#include "Gate.h"
+/*
+int Gate::andGate{
+    if (i1 == 0 || i2 == 0) {
+        output = 0;
+    }
+    if ((i1 == UNKNOWN && i2 == 1) || (i1 == 1 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
+        output = UNKNOWN;
+    }
+    else if (i1 == 1 && i2 == 1) {
+        output = 1;
+    }
+}
+
+int Gate::NandGate{
+    if (i1 == 0 || i2 == 0) {
+        output = 1;
+    }
+    if ((i1 == UNKNOWN && i2 == 1) || (i1 == 1 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
+        output = UNKNOWN;
+    }
+    else if (i1 == 1 && i2 == 1) {
+        output = 0;
+    }
+}
+
+int Gate::orGate{
+    if (i1 == 1 || i2 == 1) {
+        output == 1;
+    }
+    if ((i1 == UNKNOWN && i2 == 0) || (i1 == 0 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
+        output = UNKNOWN;
+    }
+    else if (i1 == 0 && i2 == 0) {
+        output = 0;
+    }
+}
+
+int Gate::NorGate{
+    if (i1 == 1 || i2 == 1) {
+        output == 0;
+    }
+    if ((i1 == UNKNOWN && i2 == 0) || (i1 == 0 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
+        output = UNKNOWN;
+    }
+    else if (i1 == 0 && i2 == 0) {
+        output = 1;
+    }
+}
+
+int Gate::xorGate{
+    if ((i1 == 1 && i2 == 0) || (i1 == 0 && i2 == 1)) {
+        output = 1;
+    }
+    if (i1 == i2) {
+        output = 0;
+    }
+    if (i1 == UNKNOWN || i2 == UNKNOWN) {
+        output = UNKNOWN;
+    }
+}
+
+int Gate::xNorGate{
+    if ((i1 == 1 && i2 == 0) || (i1 == 0 && i2 == 1)) {
+        output = 0;
+    }
+    if (i1 == i2) {
+        output = 1;
+    }
+    if (i1 == UNKNOWN || i2 == UNKNOWN) {
+        output = UNKNOWN;
+    }
+}
+
+int Gate::notGate{
+    if (i1 == 1) {
+        output = 0;
+    }
+    else if (i1 == 0) {
+        output = 1;
+    }
+    if (i2 == 1) {
+        output = 0;
+    }
+    else if (i2 == 0) {
+        output = 1;
+    }
+    if (i1 == UNKNOWN || i2 == UNKNOWN){
+        output = UNKNOWN;
+    }
+}
+*/
+int Gate::getDelay() const { return delay; }
+Wire* Gate::getInput1(int) const { return input1; }
+Wire* Gate::getInput2(int) const { return input2; }
+Wire* Gate::getOutput() const { return output; }
+int Gate::evaluate() const {
+    int i1 = input1->getValue();
+    int i2 = input2->getValue();
+    int returnVal;
+    if (i1 == UNKNOWN && type == NOT) returnVal = UNKNOWN; // edge cases. no reason for their order
+    else if (i1 == UNKNOWN && i2 == UNKNOWN) returnVal = UNKNOWN; // two unknowns
+    else if (i1 + i2 == 3) // One high one unknown input
+    {
+        if (type == AND) returnVal = UNKNOWN;
+        else if (type == XOR) returnVal = UNKNOWN;
+        else if (type == NAND) returnVal = UNKNOWN;
+        else if (type == XNOR) returnVal = UNKNOWN;
+
+        else if (type == OR) returnVal = HIGH;
+        else if (type == NOR) returnVal = LOW;
+    }
+    else if (i1 + i2 == 2) // One low one unknown input
+    {
+        if (type == AND) returnVal = LOW;
+        else if (type == NAND) returnVal = HIGH;
+
+        else if (type == OR) returnVal = UNKNOWN;
+        else if (type == NOR) returnVal = UNKNOWN;
+        else if (type == XOR) returnVal = UNKNOWN;
+        else if (type == XNOR) returnVal = UNKNOWN;
+    }
+    else // Do bitwise math on i1 and i2 to save the code
+    {
+        if (type == NOT) returnVal = !i1;
+        else if (type == AND) returnVal = i1 & i2;
+        else if (type == OR) returnVal = i1 | i2;
+        else if (type == XOR) returnVal = i1 ^ i2;
+        else if (type == NAND) returnVal = !(i1 & i2);
+        else if (type == NOR) returnVal = !(i1 | i2);
+        else if (type == XNOR) returnVal = !(i1 ^ i2);
+    }
+    return returnVal;
+}
---
+++ Gate.h
@@ -0,0 +1,35 @@
+#ifndef GATE_H
+
+#include "Wire.h"
+
+#define GATE_H
+#define NOT 0
+#define AND 1
+#define OR 2
+#define XOR 3
+#define NAND 4
+#define NOR 5
+#define XNOR 6
+
+#define LOW 0
+#define HIGH 1
+#define UNKNOWN 2
+
+class Gate {    
+public:
+    Gate::Gate(int type, int delay, Wire* input1, Wire* input2, Wire* output): type(type), delay(delay), input1(input1), input2(input2), output(output){};
+    int getDelay() const;
+    Wire* getInput1(int) const;
+    Wire* getInput2(int) const;
+    Wire* getOutput() const;
+    int evaluate() const;
+    
+private:
+    unsigned int type;
+    int delay;
+    Wire* input1;
+    Wire* input2;
+    Wire* output;   
+};
+
+#endif
