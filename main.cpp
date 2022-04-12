9bc55b91927379f21a9f790c724abcdc18fb80c8
arrxnov <jcbgrd@gmail.com>
2022-04-11 22:15:20 -0400

    Added args check

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main(int argc, char** argv)
 {   
-    std::cout << !1 << " " << !0 << std::endl;
+    if (argc != 2)
+    {
+        std::cout << "[+] Usage: " << argv[0] << " <CIRCUIT FILE> <VECTOR FILE>" << std::endl;
+        return 1;
+    }
     return 0;
 }
