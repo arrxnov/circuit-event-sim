3977180eb4bdeda664e2e5c626083e8d91cc5377
arrxnov <jcbgrd@gmail.com>
2022-04-11 22:11:05 -0400

    Removed old code

--- Gate.cpp
+++ Gate.cpp
@@ -1,108 +1,21 @@
 #include "Gate.h"
-/*
-int Gate::andGate{
-    if (i1 == 0 || i2 == 0) {
-        output = 0;
-    }
-    if ((i1 == UNKNOWN && i2 == 1) || (i1 == 1 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
-        output = UNKNOWN;
-    }
-    else if (i1 == 1 && i2 == 1) {
-        output = 1;
-    }
-}
 
-int Gate::NandGate{
-    if (i1 == 0 || i2 == 0) {
-        output = 1;
-    }
-    if ((i1 == UNKNOWN && i2 == 1) || (i1 == 1 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
-        output = UNKNOWN;
-    }
-    else if (i1 == 1 && i2 == 1) {
-        output = 0;
-    }
-}
-
-int Gate::orGate{
-    if (i1 == 1 || i2 == 1) {
-        output == 1;
-    }
-    if ((i1 == UNKNOWN && i2 == 0) || (i1 == 0 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
-        output = UNKNOWN;
-    }
-    else if (i1 == 0 && i2 == 0) {
-        output = 0;
-    }
-}
-
-int Gate::NorGate{
-    if (i1 == 1 || i2 == 1) {
-        output == 0;
-    }
-    if ((i1 == UNKNOWN && i2 == 0) || (i1 == 0 && i2 == UNKNOWN) || (i1 == UNKNOWN && i2 == UNKNOWN)) {
-        output = UNKNOWN;
-    }
-    else if (i1 == 0 && i2 == 0) {
-        output = 1;
-    }
-}
-
-int Gate::xorGate{
-    if ((i1 == 1 && i2 == 0) || (i1 == 0 && i2 == 1)) {
-        output = 1;
-    }
-    if (i1 == i2) {
-        output = 0;
-    }
-    if (i1 == UNKNOWN || i2 == UNKNOWN) {
-        output = UNKNOWN;
-    }
-}
-
-int Gate::xNorGate{
-    if ((i1 == 1 && i2 == 0) || (i1 == 0 && i2 == 1)) {
-        output = 0;
-    }
-    if (i1 == i2) {
-        output = 1;
-    }
-    if (i1 == UNKNOWN || i2 == UNKNOWN) {
-        output = UNKNOWN;
-    }
-}
-
-int Gate::notGate{
-    if (i1 == 1) {
-        output = 0;
-    }
-    else if (i1 == 0) {
-        output = 1;
-    }
-    if (i2 == 1) {
-        output = 0;
-    }
-    else if (i2 == 0) {
-        output = 1;
-    }
-    if (i1 == UNKNOWN || i2 == UNKNOWN){
-        output = UNKNOWN;
-    }
-}
-*/
 int Gate::getDelay() const { return delay; }
+
 Wire* Gate::getInput(int i) const 
 {
     if (i == 1) return input1;
     else return input2;
 }
+
 Wire* Gate::getOutput() const { return output; }
+
 int Gate::evaluate() const {
     int i1 = input1->getValue();
     int i2 = input2->getValue();
     int returnVal;
     if (i1 == UNKNOWN && type == NOT) returnVal = UNKNOWN; // edge cases. no reason for their order
-    else if (i1 == UNKNOWN && i2 == UNKNOWN) returnVal = UNKNOWN; // two unknowns
+    else if (i1 + i2 == 4) returnVal = UNKNOWN; // two unknowns
     else if (i1 + i2 == 3) // One high one unknown input
     {
         if (type == AND) returnVal = UNKNOWN;
