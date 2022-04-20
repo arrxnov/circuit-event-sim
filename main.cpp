8621d11c02b15c1d0c7f3b009f8e5c09c438c738
arrxnov <jcbgrd@gmail.com>
2022-04-20 09:23:53 -0400

    More user interaction housekeeping completed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "Wire.h"
+#include <fstream>
 
 int main(int argc, char** argv)
 {   
+    using namespace std;
     if (argc != 2)
     {
-        std::cout << "[+] Usage: " << argv[0] << " <CIRCUIT FILE> <VECTOR FILE>" << std::endl;
+        cout << "[+] Usage: " << argv[0] << " <CIRCUIT FILE> <VECTOR FILE>" << endl;
         return 1;
     }
+    ifstream circuitFile;
+    circuitFile.open(argv[1]);
+    if (circuitFile.fail())
+    {
+        cout << "[X] Failed to open Circuit File!" << endl;
+        return 2;
+    }
+    ifstream vectorFile;
+    vectorFile.open(argv[2]);
+    if (vectorFile.fail())
+    {
+        cout << "[X] Failed to open Vector File!" << endl;
+        return 2;
+    }
+    
     return 0;
 }
