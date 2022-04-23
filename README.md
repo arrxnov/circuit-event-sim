28db41619cdd5eedaa4264fdd28388d77b8cbf11
arrxnov <jcbgrd@gmail.com>
2022-04-22 21:50:38 -0400

    Continuing development: Read Vector Files

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Simple circuit emulator. Final project for CU CS1220
 
 arrxnov and fluffyoctopus3
+
+## Notes
+
+- The simulator will not currently handle vector events submitted out of order.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include "Wire.h"
+#include "Gate.h"
 #include <fstream>
-#include <queue>
 
 int main(int argc, char** argv)
 {   
@@ -27,6 +26,7 @@ int main(int argc, char** argv)
     
     vector<Gate*> gates;
     vector<Wire*> wires;
+    vector<string> events;
     string name;
     string type;
     string data;
@@ -36,20 +36,33 @@ int main(int argc, char** argv)
     circuitFile >> type >> name;
     while (!circuitFile.eof())
     {
-           circuitFile >> data;
-           if (data.find("INPUT") != string::npos || data.find("OUTPUT") != string::npos)
-           {
-               char* data_c = new char[data.length() + 1]; // Hack solution to make the c_string param in Wire() work with a string 
-               strcpy(data_c, data.c_str());
-               Wire* newWire = new Wire(data_c); // construct new wire with data, constructor interprets data
-           }
-           else
-           {
-               
-           }
+        circuitFile >> data;
+        if (data.find("INPUT") != string::npos || data.find("OUTPUT") != string::npos)
+        {
+            char* data_c = new char[data.length() + 1]; // Hack solution to make the c_string param in Wire() work with a string 
+            strcpy(data_c, data.c_str());
+            Wire* newWire = new Wire(data_c); // construct new wire with data, constructor interprets data
+            wires.push_back(newWire);
+        }
+        else
+        {
+            char* data_c = new char[data.length() + 1]; // Hack solution to make the c_string param in Wire() work with a string 
+            strcpy(data_c, data.c_str());
+            Gate* newGate = new Gate(data_c); // construct new wire with data, constructor interprets data
+            gates.push_back(newGate);
+        }
     }
 
     // Load in vector data and construct
+
+    string type, name;
+    int time, val;
+
+    while (!vectorFile.eof())
+    {
+        vectorFile >> data;
+        events.push_back(data);
+    }
 
     while (true)
     {
